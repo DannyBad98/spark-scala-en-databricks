11a0c65f61a8d53c522f0d114eb8195f0fe55543
DannyBad98 <dcabrerabad98@gmail.com>
2023-08-21 11:08:04 -0600

    Update Lectura 16.scala

--- Sección 4/Lectura 16.scala
+++ Sección 4/Lectura 16.scala
@@ -4,20 +4,24 @@ val sc = spark.sparkContext
 
 val rdd = sc.parallelize(Seq(1,2,3,4,5))
 
+// Resta uno a todos los elementos del RDD
 val rddResta = rdd.map(_ - 1)
 
 rddResta.collect
 
+// Devuelve un rdd con un true o false si el elemento es par
 val rddPar = rdd.map(_ % 2 == 0)
 
 rddPar.collect
 
 val rddTexto = sc.parallelize(Seq("juan", "pedro", "katia"))
 
+// Convierte todos los elementos a mayusculas
 val rddMayuscula = rddTexto.map(_.toUpperCase)
 
 rddMayuscula.collect
 
+// Concatena un hola mas el elemento del rdd
 val rddHola = rddTexto.map("Hola " + _ + "!")
 
 rddHola.collect
