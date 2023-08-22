5d66c3e25d1a24559703d07e93c6e66acde46cf0
DannyBad98 <dcabrerabad98@gmail.com>
2023-08-22 10:20:05 -0600

    Update and rename Lectura 20.scala to Lectura 20 (repartition).scala

--- Sección 4/Lectura 20 (repartition).scala
+++ Sección 4/Lectura 20 (repartition).scala
@@ -6,10 +6,12 @@ val rdd = sc.parallelize(1 to 10), 5)
 
 rdd.getNumPartitions
 
+// Aumenta de 5 a 7 el numero de particiones de rdd
 val rdd7 = rdd.repartition(7)
 
 rdd7.getNumPartitions
 
+// Reduce de 5 a 7 el numero de particiones de rdd (no recomendable, mejor usar coalesce para reducir particiones)
 val rdd3 = rdd.repartition(3)
 
 rdd3.getNumPartitions
