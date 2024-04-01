d34b102889e40f2564ae86740abc5ba06fc037cb
DannyBad98 <dcabrerabad98@gmail.com>
2024-04-01 16:48:24 -0600

    Update Lectura 35.scala

--- Sección 6/Lectura 35.scala
+++ Sección 6/Lectura 35.scala
@@ -1,4 +1,5 @@
 // Acumuladores
+//Los acumuladores son variables compartidas entre ejecutores que normalmente se utilizan para agregar contadores a su programa spark.
 
 val sc = spark.sparkContext
 
@@ -6,10 +7,13 @@ val sc = spark.sparkContext
 
 val acumuladorLong = sc.longAccumulator("longAccumulator")
 
+// Con esta linea por cada elemento del rdd se agregara su valor al acumulador, es decir, 1 + 2 + 3 + 4 + 5
 sc.parallelize(1 to 5).foreach(x => acumuladorLong.add(x))
 
+// Con esta linea se resetea el valor del acumulador
 acumuladorLong.reset
 
+// Con esta linea podemos conocer el valor del acumulador
 acumuladorLong.value
 
 acumuladorLong.isZero
@@ -18,4 +22,5 @@ acumuladorLong.isZero
 
 val acumuladorDouble = sc.doubleAccumulator("doubleAccumulator")
 
+// Con esta linea agregamos una unidad por cada elemento del rdd
 sc.parallelize(1 to 50).foreach(x => acumuladorDouble.add(1))
