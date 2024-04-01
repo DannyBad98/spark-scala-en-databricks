b12768d841d2f3fd44befe06a0ca41d27ad16d2a
DannyBad98 <dcabrerabad98@gmail.com>
2024-04-01 17:02:36 -0600

    Update Lectura 35.scala

--- Sección 6/Lectura 35.scala
+++ Sección 6/Lectura 35.scala
@@ -24,3 +24,9 @@ val acumuladorDouble = sc.doubleAccumulator("doubleAccumulator")
 
 // Con esta linea agregamos una unidad por cada elemento del rdd
 sc.parallelize(1 to 50).foreach(x => acumuladorDouble.add(1))
+
+// A diferencia de las variables regulares, los acumuladores pueden ser modificadas por los worker nodes pero solo pueden ser leidas por el driver. Esto las hace una herramienta invaluable para coleccionar y procesar informacion esencial a traves del cluster.
+
+// Ejemplo practico: https://medium.com/@ARishi/mastering-accumulators-in-apache-spark-and-not-screwing-yourself-in-the-process-8708cdb4de27#:~:text=Real%2Dlife%20Example%3A%20Monitoring%20Data%20Quality
+
+// Que es un driver?: https://keepcoding.io/blog/driver-apache-spark/
